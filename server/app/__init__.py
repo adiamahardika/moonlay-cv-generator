85e3cd9c2fc49f94b0e1b1e8ced23f2a19b8c469
William Evan Bunaidy <williambunaidy3@gmail.com>
2025-02-12 18:53:57 +0700

    mantis template

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,20 +1,27 @@
+# app/__init__.py
+
 from flask import Flask
-from dotenv import load_dotenv
+from .record_blueprint import record_blueprint
+from .bot_blueprint import bot_blueprint
+from .cv_blueprint import cv_blueprint
+from .polling_blueprint import start_polling
 from flask_cors import CORS
 import os
 
-# Load environment variables
-load_dotenv()
-
-# Initialize the Flask app
-
 
 def create_app():
-    app = Flask(__name__, static_folder='static')
-    CORS(app)
+    static_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static')
 
-    # Register the quotation blueprint from the correct path
-    from app.routes.quotation_routes import quotation_bp
-    app.register_blueprint(quotation_bp)
+  # Set the static folder path
+    app = Flask(__name__, static_folder=static_folder_path)
 
+    CORS(app)  # Enable CORS for all routes
+
+    # Register blueprints
+    app.register_blueprint(record_blueprint)
+    app.register_blueprint(bot_blueprint)
+    app.register_blueprint(cv_blueprint)
+
+    # Additional configuration can go here
+    start_polling()  # This will initiate polling in a separate thread
     return app
